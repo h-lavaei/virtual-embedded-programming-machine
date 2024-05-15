88b27fa1d4313d5e1a2db15e9a53696d181bcbeb
hosein <hosein.lavaei.06@gmail.com>
2024-05-15 20:42:54 +0330

    beta

--- src/devices.rs
+++ src/devices.rs
@@ -1,5 +1,3 @@
-use std::fmt::Pointer;
-use iced::widget::shader::wgpu::core::device::DeviceError;
 use crate::seven_segment::SevenSegment;
 
 #[derive(Debug)]
@@ -10,30 +8,27 @@ impl Device {
     pub fn print(&self){
         match self {
             Device::SevenSegment(seven_segment) => { seven_segment.print() },
-            _ => {}
         }
     }
     pub fn name(&self) -> &str{
         match self {
             Device::SevenSegment(seven_segment) => {seven_segment.name.as_str()}
-            _ => {""}
         }
     }
-    pub fn edit(&mut self,change:&str) -> Result<(),DeviceError>{
+    pub fn edit(&mut self,change:&str) -> Result<(),&str>{
         match self {
             Device::SevenSegment(seven_segment) => {
                 let mut chars = change.chars();
                 if change.chars().count() == 8 {
-                    seven_segment.leds = [if chars.nth(0).unwrap() == '1' { true } else { false }, if chars.nth(0).unwrap() == '1' { true } else { false }, if chars.nth(0).unwrap() == '1' { true } else { false }, if chars.nth(0).unwrap() == '1' { true } else { false }, if chars.nth(0).unwrap() == '1' { true } else { false }, if chars.nth(0).unwrap() == '1' { true } else { false }, if chars.nth(0).unwrap() == '1' { true } else { false }, if chars.nth(0).unwrap() == '1' { true } else { false }, ];
+                    seven_segment.leds = [chars.nth(0).unwrap() == '1',chars.nth(0).unwrap() == '1',chars.nth(0).unwrap() == '1',chars.nth(0).unwrap() == '1',chars.nth(0).unwrap() == '1',chars.nth(0).unwrap() == '1',chars.nth(0).unwrap() == '1',chars.nth(0).unwrap() == '1',];
                     Ok(())
                 } else if change.chars().count() == 1 {
                     seven_segment.make_char(change.chars().nth(0).unwrap());
                     Ok(())
                 } else {
-                    Err(DeviceError::Invalid)
+                    Err("")
                 }
             }
-            _ => {Err(DeviceError::WrongDevice)}
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -43,9 +43,9 @@ fn match_input(devices:&mut Vec<Device>){
         "edit" => {
             let name = arguments.next();
             if name.is_some() {
-                let mut device = devices.iter_mut().find(|device| device.name() == name.unwrap());
+                let device = devices.iter_mut().find(|device| device.name() == name.unwrap());
                 if device.is_some() {
-                    let mut device: &mut Device = device.unwrap();
+                    let device: &mut Device = device.unwrap();
                     let change = arguments.next();
                     if change.is_some(){
                         let change = change.unwrap();
@@ -62,8 +62,17 @@ fn match_input(devices:&mut Vec<Device>){
                 println!("Device not found!");
             }
         },
+        "remove" => {
+            let name = arguments.next();
+            if name.is_some(){
+                let device = devices.iter().find(|device| device.name() == name.unwrap()).unwrap();
+                let device_pos = devices.iter().position(|x| x.name() == device.name());
+                devices.remove(device_pos.unwrap());
+            }
+        },
         "list" => {devices.iter().for_each(move |device| device.print())},
         "help" => {println!("add a new device : add device_type name\nedit a device : edit name change\nlist devices : list\nhelp : help")},
-        _ => {},
+        "exit" => {std::process::exit(0)}
+        _ => {println!("Command not found! See help for valid commands")},
     }
 }
